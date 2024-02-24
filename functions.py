f0bfd4af7d1bd75bd2ebf91657c71ccbef672999
dan <tuj88978@temple.edu>
2024-02-24 15:51:22 -0800

    Initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+def get_todos(filepath="todos.txt"):
+    with open(filepath, 'r') as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todo_arg, filepath='todos.txt'):
+    with open(filepath,'w') as file:
+        file.writelines(todo_arg)
+if __name__ == "__main__":
+    print("Hello")
+    print(get_todos())
